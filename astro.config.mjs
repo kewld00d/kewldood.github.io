b993e85e5de72455a609b3dbda424a2df03383d3
kewld00d <80879407+kewld00d@users.noreply.github.com>
2026-04-06 10:32:34 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nicolazzzz.github.io',
+    site: 'https://kewldood.github.io',
   integrations: [mdx(), sitemap()],
 
   vite: {
